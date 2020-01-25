278ad04fb59d16fb6fc8a358344508738ff912e8
Aneta Dudek <aneta.dudek.89@gmail.com>
2020-01-25 13:44:40 +0100

    Add colors to actions

---
+++ colors.py
@@ -0,0 +1,22 @@
+from colored import fg, attr
+
+
+# general
+RESET = attr('reset')
+
+# board.py
+PLAYER = fg('cyan_1')
+MONSTER = fg('light_red')
+ITEM = fg('light_yellow')
+EVENT = fg('light_green')
+GATE = fg('orchid_1')
+EMPTY = fg('dark_green')
+WALL = fg('dark_gray')
+
+# battle.py
+ENEMY = fg('orange_red_1')
+PLAYER = fg('chartreuse_2a')
+ACTION = fg('dark_violet_1a')
+
+
+
