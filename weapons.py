3e811bedb62d0818a8d2ee70d92d6aacb80ef88d
ntiany <krolkas@gmail.com>
2020-01-26 13:50:24 +0100

    Items with corpo descriptions

--- weapons.py
+++ weapons.py
@@ -11,9 +11,9 @@ class WeaponsPool():
 
 # weapon_properties = [name, attack, critical_chance, status]
     weapons = [Weapon(["Fists", 1, 0, "Your own fists"]),
-           Weapon(["Dagger", 2, 0, "Shiny dagger with"]),
-           Weapon(["Simple sword", 3, 1, "Sturdy piece of iron for monster slaying"]),
-           Weapon(["Axe", 3, 2, "You could chop monster in half with this"]),
-           Weapon(["Hammer", 4, 1, "Smashing machine"])]
+           Weapon(["Fork", 2, 0, "Shiny fork"]),
+           Weapon(["Broom", 3, 1, "Sturdy piece of wood for punishing"]),
+           Weapon(["Scissors", 3, 2, "You could chop monster in half with this"]),
+           Weapon(["Boss badge", 4, 1, "Smashing machine"])]
 
     weapon_names = [weapon.name for weapon in weapons]
